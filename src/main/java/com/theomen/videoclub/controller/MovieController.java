8327759081d92322a1f94901fcae114e22c2ab39
AlbaGG95 <albaganduxe95@gmail.com>
2026-03-26 12:54:32 +0100

    feat: add MovieController with CRUD REST endpoints

--- src/main/java/com/theomen/videoclub/controller/MovieController.java
+++ src/main/java/com/theomen/videoclub/controller/MovieController.java
@@ -1,4 +1,50 @@
 package com.theomen.videoclub.controller;
 
+import com.theomen.videoclub.model.Movie;
+import com.theomen.videoclub.service.MovieService;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping
+    public Movie saveMovie(@RequestBody Movie movie) {
+        return movieService.saveMovie(movie);
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Long id) {
+        return movieService.getMovieById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        return movieService.updateMovie(id, movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return "Movie deleted successfully";
+    }
 }
