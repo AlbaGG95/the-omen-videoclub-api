24d2d1e74cf7965d31e78e99c11905c7ca5fd8d5
AlbaGG95 <albaganduxe95@gmail.com>
2026-03-26 12:48:38 +0100

    feat: add MovieService interface with CRUD methods

--- src/main/java/com/theomen/videoclub/service/MovieService.java
+++ src/main/java/com/theomen/videoclub/service/MovieService.java
@@ -1,4 +1,17 @@
 package com.theomen.videoclub.service;
 
-public class MovieService {
+import com.theomen.videoclub.model.Movie;
+import java.util.List;
+
+public interface MovieService {
+
+    Movie saveMovie(Movie movie);
+
+    List<Movie> getAllMovies();
+
+    Movie getMovieById(Long id);
+
+    Movie updateMovie(Long id, Movie movie);
+
+    void deleteMovie(Long id);
 }
