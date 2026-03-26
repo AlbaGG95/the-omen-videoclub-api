cc00ec71baa28126af8c3d9968ac3f900d08873b
AlbaGG95 <albaganduxe95@gmail.com>
2026-03-26 12:27:41 +0100

    chore: replace broken Gradle base with clean Spring Boot Maven project

---
+++ src/main/java/com/theomen/videoclub/TheOmenVideoclubApiApplication.java
@@ -0,0 +1,13 @@
+package com.theomen.videoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOmenVideoclubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOmenVideoclubApiApplication.class, args);
+	}
+
+}
